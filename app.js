51343648bd5755068b55e1949532c1938af1a873
Eric D. Torres <etorresnotary@gmail.com>
2020-09-06 20:27:24 -0500

    commenting the code

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const readBtn = document.querySelector("#read-text");
 const toggleBtn = document.querySelector(".toggle-btn");
 const closeBtn = document.querySelector("#close-btn");
 
+//hard coded objects for image and text to read with that image
 const data = [
   {
     image: "./assets/img/drink.jpg",
@@ -57,32 +58,41 @@ const data = [
   },
 ];
 
+//running a for each on the data array
 data.forEach(createBox);
 
 //create boxes for image and text
 function createBox(item) {
+  //creating a variable to hold the created div html element
   const box = document.createElement("div");
 
+  //deconstructing the item and pulling out the image and text into variables
   const { image, text } = item;
 
+  //adding the box class to the new html element
   box.classList.add("box");
+  //setting the innerHTML of the div in box variable to display the deconstructed image and text
   box.innerHTML = `
         <img src="${image}" alt="${text}"/>
         <p class="box-info">${text}</p>
     `;
 
-    //TODO -- speak event 
+    //event listener for speak eventspeak event 
     box.addEventListener("click", () => {
+      //running set text 
         setTextMessage(text);
+        //running function to make computer talk!!!
         speakText();
 
         //add an active effect
         box.classList.add("active");
+        //setTimeout to remove the active shadow form the selected box
         setTimeout(() => {
             box.classList.remove("active")
         }, 1200);
     })
 
+    //appending the box variable onto the main container
     mainEl.appendChild(box);
 }
 
@@ -94,30 +104,39 @@ let voices = [];
 
 //populate select element in HTML with options from Speech Synthesis
 function getVoices() {
+  //setting variable voices to go into the speechSynthesis API and pull out the available voices
     voices = speechSynthesis.getVoices();
 
+    //using forEach on voices to populate the options for the select
     voices.forEach(voice => {
+      //setting the option HTML element into the variable named option
     const option = document.createElement("option");
 
+    //pulling out the name value and setting it to the option value
     option.value = voice.name;
+    //setting the option's innerText to be the name and teh language
     option.innerText = `${voice.name} ${voice.lang}`
 
+    //putting the options into the select in the html
     voiceSelectEl.appendChild(option)
     })
 }
 
 //set text
 function setTextMessage(text) {
+  //setting the text of the message to synthesize
     message.text = text;
 }
 
 //speak the text
 function speakText() {
+  //making the computer speak!!
     speechSynthesis.speak(message);
 }
 
 //set foice to use to read text
 function setVoice(e) {
+  //setting new voice from selected option in html select
     message.voice = voices.find(voice => voice.name === e.target.value);
 }
 
@@ -127,10 +146,12 @@ speechSynthesis.addEventListener("voiceschanged", getVoices);
 //event listeners
 //toggle text box
 toggleBtn.addEventListener("click", ()=>{
+  //putting the show class on the text box
     document.querySelector("#text-box").classList.toggle("show")
 });
 //close button on text box
 closeBtn.addEventListener("click", ()=>{
+  //removing the show class on the text box
     document.querySelector("#text-box").classList.remove("show")
 });
 
@@ -139,8 +160,11 @@ voiceSelectEl.addEventListener("change", setVoice);
 
 //read text from text area
 readBtn.addEventListener("click", () => {
+  //running the function to set the message from the text box textarea
     setTextMessage(textAreaEl.value);
+    //making the computer SPEAK!!
     speakText();
 })
 
+//running the funciton to populate the select with options in the html
 getVoices();
